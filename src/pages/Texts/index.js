063792e2598c5c7e7e780f898bab89035e7c5f41
Carlos Villuendas <carlosvillu@gmail.com>
2017-07-07 00:48:34 +0200

    feat(pages): [Texts] add drawer control

--- src/pages/Texts/index.js
+++ src/pages/Texts/index.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import {PropTypes} from 'react'
+import Texts from './component'
 
-const Texts = () => (
-  <div>
-    Texts
-  </div>
-)
+import {compose, mapUIServiceToProps, mapUIResponseToProps} from '@schibstedspain/ddd-react-redux'
+import withHandlers from 'recompose/withHandlers'
+import getContext from 'recompose/getContext'
 
-export default Texts
+export default compose(
+  mapUIServiceToProps('drawer'),
+  mapUIResponseToProps('drawer'),
+  getContext({
+    i18n: PropTypes.object
+  }),
+  withHandlers({
+    toggleDrawer: ({drawerUI, setDrawerUI}) => () => setDrawerUI(!drawerUI)
+  })
+)(Texts)
