670f837ecd3b7a20c5303c6b5a61d6baa40a7b22
Carlos Villuendas <carlosvillu@gmail.com>
2017-07-07 00:47:54 +0200

    feat(components): [Root] add styles

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -1,7 +1,24 @@
 import React from 'react'
 
-const Root = () => (
-  <div>Root</div>
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+
+import Drawer from '../Drawer'
+
+import {Container} from './styles'
+
+const Root = ({children}) => (
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <Container>
+      <Drawer />
+      {children}
+    </Container>
+  </ MuiThemeProvider>
 )
+
+Root.displayName = 'Root'
+Root.propTypes = {
+  children: React.PropTypes.element
+}
 
 export default Root
