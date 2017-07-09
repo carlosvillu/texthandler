6ce073311e9b57ac72d046a823f7059501db6fa7
Carlos Villuendas <carlosvillu@gmail.com>
2017-07-09 23:57:53 +0200

    feat(literals): Form literals

--- src/literals/en-GB.js
+++ src/literals/en-GB.js
@@ -1,6 +1,19 @@
 export default {
+  CANCEL: 'Cancel',
+  COURSE: 'Course',
+  COURSE_2: '2º Course',
+  COURSE_4: '4º Course',
+  COURSE_8: '8º Course',
+  FILES_SELECTS: 'Files uploads',
+  FOLLOW_UP_ONE: 'Follow up 1',
+  FOLLOW_UP_TWO: 'Follow up 2',
+  FRECUENCY: 'Frecuency',
+  MAINTENANCE_TEST: 'Maintenance test',
+  POSTEST: 'Postest',
+  PRETEST: 'Pretest',
+  SEQUENCY: 'Sequency',
+  SUBMIT: 'Submit',
   TITLE: 'TextHandler',
   UPLOAD_DIALOG_TITLE: 'Upload texts',
-  CANCEL: 'Cancel',
-  SUBMIT: 'Submit'
+  UPLOAD_TEXTS: 'Upload texts'
 }
