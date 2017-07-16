60506612f63c91372cf1071b7eb87ccec6cf8109
Carlos Villuendas <carlosvillu@gmail.com>
2017-07-16 20:02:51 +0200

    feat(pages): [Texts] list texts

--- src/pages/Texts/component.js
+++ src/pages/Texts/component.js
@@ -2,19 +2,24 @@ import React, {PropTypes} from 'react'
 
 import AppBar from 'material-ui/AppBar'
 
-import {Container} from './styles'
+import {Container, ContainerListTexts} from './styles'
 import UploadTextsDialog from '../../components/UploadTextsDialog'
+import ListTexts from '../../components/ListTexts'
 
-const Texts = ({toggleDrawer, i18n}) => (
-  <Container>
-    <AppBar
-      title={i18n.t('TITLE')}
-      onLeftIconButtonTouchTap={toggleDrawer}
-    />
-    <UploadTextsDialog />
-    Texts
-  </Container>
-)
+const Texts = ({toggleDrawer, i18n}) => {
+  return (
+    <Container>
+      <AppBar
+        title={i18n.t('TITLE')}
+        onLeftIconButtonTouchTap={toggleDrawer}
+      />
+      <UploadTextsDialog />
+      <ContainerListTexts>
+        <ListTexts />
+      </ContainerListTexts>
+    </Container>
+  )
+}
 
 Texts.displayName = 'Texts'
 Texts.propTypes = {
--- src/pages/Texts/styles.js
+++ src/pages/Texts/styles.js
@@ -1,5 +1,12 @@
 import {styled} from 'styletron-react'
 
+export const ContainerListTexts = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  marginTop: '25px'
+})
+
 export const Container = styled('div', props => ({
   color: 'brown',
   fontSize: '12px',
