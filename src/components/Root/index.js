ec6a29bf13d69dfe0eebc7e8df549e48c655f31c
Carlos Villuendas <carlosvillu@gmail.com>
2017-07-16 20:00:01 +0200

    feat(components): [Root] theming the app

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -7,8 +7,24 @@ import Drawer from '../Drawer'
 
 import {Container} from './styles'
 
+import {
+  teal900, red500
+} from 'material-ui/styles/colors'
+
+/**
+ *  Light Theme is the default theme used in material-ui. It is guaranteed to
+ *  have all theme variables needed for every component. Variables not defined
+ *  in a custom theme will default to these values.
+ */
+const palette = {
+  palette: {
+    primary1Color: teal900,
+    primary2Color: red500
+  }
+}
+
 const Root = ({children}) => (
-  <MuiThemeProvider muiTheme={getMuiTheme()}>
+  <MuiThemeProvider muiTheme={getMuiTheme(palette)}>
     <Container>
       <Drawer />
       {children}
